cbabbcead83df786a2db65ebe129f0edbe130a30
Sean <craig.sean92@gmail.com>
2022-05-29 13:28:38 -0600

    commit tree f3485dcf0c5de15a632001c9692e7a775404fb4f

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,21 @@
+require "active_record"
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/test.sqlite3"
+  )
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT
+  )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+class Dog < ActiveRecord::Base
+end
+  
+
+
 class Dog
 end
